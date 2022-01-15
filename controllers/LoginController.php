de69ecd5dd96b814a63bee89f2f42b21ebe6cc0a
Julio Cefnar Auyon R <juliorecinos30@gmail.com>
2022-01-15 01:38:24 -0600

    V.450 Creando Routing y Controller

---
+++ controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+
+class LoginController{
+   public static function login(){
+       echo "Desde logi";
+   }
+
+   public static function logout(){
+       echo "Desde Logou";
+   }
+
+   public static function olvide (){
+       echo "Desde olvid";
+   }
+
+   public static function recuperar(){
+       echo "Desde recupera";
+   }
+
+   public static function crear(){
+       echo "Hola desde crea";
+   }
+
+}
